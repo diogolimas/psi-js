898d81046f30fe9570ee7f203cf0f965b390c1f3
diogolimas <diogo.libras43@gmail.com>
2019-11-01 14:26:51 -0300

    update

---
+++ jsfirst.js
@@ -0,0 +1,10 @@
+function definirCores(){
+    var paragrafos = document.getElementsByTagName("p");
+    var cores = ["blue", "red", "green", "yellow"];
+    var i;
+    for (i=0; i<paragrafos.length; i++){
+        paragrafos[i].style.backgroundColor = cores[i];
+    }
+}   
+
+
