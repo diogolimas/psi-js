9d96d37333857a8b0462529e51763679147b7c0d
Diogo da Silva Lima <diogo.s@academico.ifrn.edu.br>
2019-11-07 18:38:51 -0200

    update

--- verifica.js
+++ verifica.js
@@ -7,5 +7,20 @@ function verificarSenha() {
         var msg = document.getElementById('mensagem');
         msg.innerHTML = "As senhas não conferem";
         msg.className = "alert alert-danger";
+        return false;
+    }
+    return true;
+}
+function validar(){
+    var campo1 = document.getElementById("senha");
+    var campo2 = document.getElementById("confirmarsenha");
+    if(campo1.value != campo2.value){
+        campo2.setCustomValidity("As senhas não conferem");
+        campo2.validity = false;        
+    }else{
+        
+        campo2.setCustomValidity("");
+        campo2.validity = true;
+        
     }
 }
